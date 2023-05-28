babd17c70c543612fc94f5f7c3ef4f9f56b3d1d5
SirSZ8 <127765389+SirSZ8@users.noreply.github.com>
2023-05-28 12:49:35 +0700

    commit crud

---
+++ models/Barang.js
@@ -0,0 +1,10 @@
+// const mongoose = require("./connector");
+
+// const BarangSchema = mongoose.Schema({
+//   namaBarang: String,
+//   hargaBarang: Number,
+// });
+
+// const Barang = mongoose.model("Barang", BarangSchema);
+
+// module.exports = BarangSchema;
