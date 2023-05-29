52c891a85986f7cb1b215d3cc3b72ec0b43e0f3f
SirSZ8 <127765389+SirSZ8@users.noreply.github.com>
2023-05-29 20:26:24 +0700

    add put and post in controlleruser.js

--- controllers/ControllerUser.js
+++ controllers/ControllerUser.js
@@ -1,3 +1,5 @@
+const ModelUser = require("../models/ModelUser");
+
 const ControllerUser = require("express").Router();
 
 ControllerUser.get("/", (req, res) => {
@@ -12,6 +14,28 @@ ControllerUser.get("/zeas", (req, res) => {
     username: "asta",
     email: "SirSZ@gmail.com",
   });
+});
+
+ControllerUser.post("/", async (req, res) => {
+  const payload = req.body;
+
+  const result = await ModelUser.create(
+    payload.firstName,
+    payload.lastName,
+    payload.email,
+    payload.password
+  );
+
+  return res.status(201).json(result);
+});
+
+ControllerUser.put("/:email", async (req, res) => {
+  const result = await ModelUser.edit(
+    req.body.firstName,
+    req.body.lastName,
+    req.params.email
+  );
+  return res.status(200).json(result);
 });
 
 module.exports = ControllerUser;
